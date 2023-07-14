7b6a9b87f6b7629223a245edf3d525a2cba3923a
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-07-14 04:04:13 +0300

    unit testing test cases

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python3
+"""Unittest for base_model.py"""
+
+import unittest
+from models.base_model import BaseModel
+from datetime import datetime
+
+
+class TestBaseModel(unittest.TestCase):
+    """Test cases for base_model.py"""
+
+    def tearDown(self):
+        """Tears down test methods."""
+        self.resetStorage()
+        pass
+
+    def test_instantiation(self):
+        """Tests instantiation of BaseModel class"""
+
+        bm = BaseModel()
+        self.assertEqual(str(type(bm)),
+                         "<class 'models.base_model.BaseModel'>")
+        self.assertIsInstance(bm, BaseModel)
+        self.assertTrue(issubclass(type(bm), BaseModel))
+
+    def test_id(self):
+        """Tests id attribute"""
+
+        bm = BaseModel()
+        self.assertTrue(hasattr(bm, "id"))
+        self.assertIsInstance(bm.id, str)
+
+    def test_created_at(self):
+        """Tests created_at attribute"""
+
+        bm = BaseModel()
+        self.assertTrue(hasattr(bm, "created_at"))
+        self.assertIsInstance(bm.created_at, datetime)
+
+    def test_updated_at(self):
+        """Tests updated_at attribute"""
+
+        bm = BaseModel()
+        self.assertTrue(hasattr(bm, "updated_at"))
+        self.assertIsInstance(bm.updated_at, datetime)
+
+    def test_save(self):
+        """Tests save method"""
+
+        bm = BaseModel()
+        bm.save()
+        self.assertNotEqual(bm.created_at, bm.updated_at)
+
+    def test_str(self):
+        """Tests __str__ method"""
+
+        bm = BaseModel()
+        bm_str = bm.__str__()
+        self.assertEqual(bm_str,
+                         "[BaseModel] ({}) <{}>".format(bm.id, bm.__dict__))
+
+    def test_to_dict(self):
+        """Tests to_dict method"""
+
+        bm = BaseModel()
+        bm_dict = bm.to_dict()
+        self.assertEqual(bm_dict["__class__"], "BaseModel")
+        self.assertEqual(type(bm_dict["created_at"]), str)
+        self.assertEqual(type(bm_dict["updated_at"]), str)
+
+    def test_init_from_dict(self):
+        """Tests init from dictionary"""
+
+        bm = BaseModel()
+        bm_dict = bm.to_dict()
+        bm2 = BaseModel(**bm_dict)
+        self.assertEqual(bm.id, bm2.id)
+        self.assertEqual(bm.created_at, bm2.created_at)
+        self.assertEqual(bm.updated_at, bm2.updated_at)
+        self.assertEqual(bm.__dict__, bm2.__dict__)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Test File Storage
+"""
+
+import unittest
+from models.engine.file_storage import FileStorage
+
+
+class test_fileStorage(unittest.TestCase):
+    """Test FileStorage Class"""
+
+    def test_instances(self):
+        """Test Instantiation"""
+        obj = FileStorage()
+        self.assertIsInstance(obj, FileStorage)
+
+    def test_docs(self):
+        """Test function definitions"""
+        self.assertIsNotNone(FileStorage.all)
+        self.assertIsNotNone(FileStorage.new)
+        self.assertIsNotNone(FileStorage.save)
+        self.assertIsNotNone(FileStorage.reload)
+
+if __name__ == '__main__':
+    unittest.main()
