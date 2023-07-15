4f3cf6134969ba0f422fd95aa63076c635c5d6b6
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-07-15 22:57:04 +0300

    add unittest for BaseModel with FileStorage

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -133,6 +133,22 @@ class TestBaseModel(unittest.TestCase):
             f.seek(0)
             self.assertEqual(json.loads(f.read()), file_content)
 
+    def test_save_no_args(self):
+        """Tests save() with no arguments."""
+        self.resetStorage()
+        with self.assertRaises(TypeError) as e:
+            BaseModel.save()
+        msg = "save() missing 1 required positional argument: 'self'"
+        self.assertEqual(str(e.exception), msg)
+
+    def test_save_excess_args(self):
+        """Tests save() with too many arguments."""
+        self.resetStorage()
+        with self.assertRaises(TypeError) as e:
+            BaseModel.save(self, 98)
+        msg = "save() takes 1 positional argument but 2 were given"
+        self.assertEqual(str(e.exception), msg)
+
 
 
 if __name__ == "__main__":
