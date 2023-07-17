d1ab66f86163d3b4b1d46f11b0cc7497ee12a254
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-07-18 01:20:28 +0300

    add unit tests for file storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -5,6 +5,7 @@ Test File Storage
 
 import unittest
 from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
 
 
 class test_fileStorage(unittest.TestCase):
@@ -21,6 +22,39 @@ class test_fileStorage(unittest.TestCase):
         self.assertIsNotNone(FileStorage.new)
         self.assertIsNotNone(FileStorage.save)
         self.assertIsNotNone(FileStorage.reload)
+
+    def test_file_path(self):
+        """Test file path"""
+        obj = FileStorage()
+        self.assertIsInstance(obj._FileStorage__file_path, str)
+
+    def test_objects(self):
+        """Test objects"""
+        obj = FileStorage()
+        self.assertIsInstance(obj._FileStorage__objects, dict)
+
+    def test_all(self):
+        """Test all"""
+        obj = FileStorage()
+        self.assertIsInstance(obj.all(), dict)
+
+    def test_new(self):
+        """Test new"""
+        obj = FileStorage()
+        obj.new(BaseModel())
+        self.assertTrue(obj.all())
+
+    def test_save(self):
+        """Test save"""
+        obj = FileStorage()
+        obj.save()
+        self.assertTrue(obj.all())
+
+    def test_reload(self):
+        """Test reload"""
+        obj = FileStorage()
+        obj.reload()
+        self.assertTrue(obj.all())
 
 
 if __name__ == '__main__':
